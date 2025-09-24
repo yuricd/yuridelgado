d2cef65f49d440df3d27a169a728de595031c163
Yuri <6846623+yuricd@users.noreply.github.com>
2025-09-24 18:08:49 -0300

    feat: create techlist component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,10 +9,10 @@ export default function Button({ children, className }: ButtonProps) {
   return (
     <button
       className={cn(
-        `cursor-pointer text-[1rem] font-semibold text-nowrap py-2.5 px-5 rounded-sm text-main-black
+        `cursor-pointer text-[1rem] font-semibold text-nowrap py-2 px-5 rounded-sm text-main-black border-2 border-transparent
           hover:brightness-95 
           active:brightness-75 
-          focus:border-2 focus:border-gray-700
+          focus:border-white
           transition-all duration-150`,
         className
       )}
---
+++ src/components/TechList/TechList.tsx
@@ -0,0 +1,122 @@
+import * as HoverCard from "@radix-ui/react-hover-card";
+import { Icon } from "@iconify/react";
+import { Typography } from "../Typography/Typography";
+import type { PropsWithChildren } from "react";
+
+const techIcons = [
+  {
+    iconName: "simple-icons:react",
+    name: "React",
+    description:
+      "My go-to solution for SPAs. It’s flexible, has a great and huge community, and it’s used and supported by many frameworks, such as Next.js and Astro.",
+  },
+  {
+    iconName: "simple-icons:nodedotjs",
+    name: "Node.js",
+    description:
+      "High I/O capabilities, huge community with a myriad with different packages, an easy to maintain in a cross-functional team.",
+  },
+  {
+    iconName: "simple-icons:tsnode",
+    name: "TypeScript",
+    description:
+      "JavaScript is becoming the lingua franca, and Typescript is its superset to bring static types to it. I can’t even remember when was the last time I built a JS app without Typescript.",
+  },
+  {
+    iconName: "simple-icons:nextdotjs",
+    name: "Next.js",
+    description:
+      "For powerful and complex SSR websites in React, with a strong and growing community.",
+  },
+  {
+    iconName: "simple-icons:python",
+    name: "Python",
+    description:
+      "Time-proven technology. When it comes to data workloads and AI, Python rocks. For general-purpose or CPU-heavy tasks, Python is a good candidate.",
+  },
+  {
+    iconName: "simple-icons:astro",
+    name: "Astro",
+    description:
+      "One of my newest and happiest findings. It’s an open-source framework that allows both static and SSR, and it has out of the box support to frameworks like Vue, Svelte, and React.",
+  },
+  {
+    iconName: "simple-icons:tailwindcss",
+    name: "Tailwind",
+    description:
+      "Allows to build responsive UIs quickly, without even touching CSS. It supports many utilities and customization with a seamless developer experience.",
+  },
+  {
+    iconName: "simple-icons:postgresql",
+    name: "PostgreSQL",
+    description:
+      "Not all applications are suitable for a relational database, but for all the others I choose Postgres. It’s open-source, has powerful extensions, rich data types, and full-text search support. ",
+  },
+  {
+    iconName: "simple-icons:expo",
+    name: "Expo",
+    description:
+      "An open-source platform to build native apps for Android and iOS. It makes the development and integration of mobile apps with app stores much easier. ",
+  },
+  {
+    iconName: "simple-icons:immersivetranslate",
+    name: "i18n",
+    description:
+      "If you need to ship your app globally, internationalization is a must. Adding multi-lang support either in your front or backend is possible with i18n techniques.",
+  },
+  {
+    iconName: "hugeicons:ai-network",
+    name: "AI",
+    description:
+      "It’s a new era and AI can power your app, either in the client side or as a backoffice tool to streamline processes.",
+  },
+  {
+    iconName: "hugeicons:more-horizontal-square-02",
+    name: "More",
+    description:
+      "My approach is grounded in continuous learning, enabling me to select the most effective tools for each unique problem. I believe in having a diverse toolkit - not every problem is a nail, and I strive to be prepared with the right solution whether.",
+  },
+];
+
+interface PopoverButtonProps extends PropsWithChildren {
+  name: string;
+}
+
+function PopoverButton({ name, children }: PopoverButtonProps) {
+  return (
+    <HoverCard.Root openDelay={0} closeDelay={0}>
+      <HoverCard.Trigger asChild>{children}</HoverCard.Trigger>
+
+      <HoverCard.Content
+        side="top"
+        align="center"
+        sideOffset={6}
+        className="bg-gray-800 text-white text-sm rounded-md px-2 py-1 shadow-lg select-none"
+      >
+        <Typography variant="body1" as="p">
+          {name}
+        </Typography>
+        <HoverCard.Arrow className="fill-gray-800" />
+      </HoverCard.Content>
+    </HoverCard.Root>
+  );
+}
+
+export default function TechList() {
+  return (
+    <div className="flex gap-8">
+      {techIcons.map(({ iconName, name }) => (
+        <PopoverButton key={name} name={name}>
+          <button className="opacity-25 hover:opacity-90 transition-opacity duration-300">
+            <Icon
+              icon={iconName}
+              width="48"
+              height="48"
+              className="text-gray-300"
+            />
+          </button>
+        </PopoverButton>
+      ))}
+    </div>
+  );
+}
