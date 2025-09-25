8bc9a06be134145f4d4f0dddda4356c0ec249e16
Yuri <6846623+yuricd@users.noreply.github.com>
2025-09-25 10:30:06 -0300

    feat: add let's talk banner

---
+++ src/components/LetsTalkBanner/LetsTalkBanner.tsx
@@ -0,0 +1,19 @@
+import Button from "../Button/Button";
+import { Typography } from "../Typography/Typography";
+
+export default function LetsTalkBanner() {
+  return (
+    <div className="flex w-full bg-brand-primary py-6">
+      <div className="flex justify-center items-center w-5xl max-w-11/12 mx-auto gap-4">
+        <Typography
+          variant="header3"
+          as="h3"
+          className="text-main-black font-medium text-center"
+        >
+          Have something in mind?
+        </Typography>
+        <Button className="bg-white">Let's talk</Button>
+      </div>
+    </div>
+  );
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,57 +7,82 @@ const projects = [
       "OrbiGuide - Discover places, optimize routes, and learn about each spot around the world.",
     link: "orbiguide.com",
     stack: ["FastAPI", "Postgres", "React Native", "Expo", "React", "Astro"],
+    image: {
+      path: "/images/orbiguide-screenshot.webp",
+      width: 955,
+      height: 775,
+      alt: "OrbiGuide screenshot",
+    },
   },
 ];
 
 export default function Projects() {
   return (
-    <div className="flex flex-col items-center w-5xl max-w-11/12 mx-auto gap-16">
-      <Typography
-        variant="header3"
-        as="h2"
-        className="border border-gray-100 rounded-full px-6 py-2"
-      >
-        My projects
-      </Typography>
+    <div>
+      <div className="flex flex-col items-center w-5xl max-w-11/12 mx-auto gap-16 mb-16">
+        <Typography
+          variant="body1"
+          as="h2"
+          className="border border-gray-100 rounded-full px-4 py-1.5 font-medium"
+        >
+          My projects
+        </Typography>
+      </div>
 
-      {projects.map(({ title, link, stack }) => (
-        <div key={title} className="flex flex-col gap-4">
-          <Typography variant="header1" as="h3">
-            {title}
-          </Typography>
+      {projects.map(({ title, link, stack, image }) => {
+        const imageH = image.height / 3;
 
-          <div className="flex gap-4">
-            <Typography
-              variant="link"
-              as="a"
-              className="text-gray-100 flex gap-1 items-center"
+        return (
+          <div key={title} className="flex flex-col gap-4">
+            <div
+              className="flex flex-col w-5xl max-w-11/12 mx-auto gap-4"
+              style={{ paddingBottom: imageH + 48 }}
             >
-              <Icon
-                icon="ci:external-link"
-                width="18"
-                height="18"
-                className="text-gray-100"
-              />
-              {link}
-            </Typography>
+              <Typography variant="header1" as="h3">
+                {title}
+              </Typography>
 
-            <div className="flex gap-1">
-              {stack.map((tech) => (
-                <span key={tech} className="px-3 py-1 rounded-full bg-white/10">
-                  {tech}
-                </span>
-              ))}
+              <div className="flex gap-4">
+                <Typography
+                  variant="link"
+                  as="a"
+                  className="text-gray-100 flex gap-1 items-center"
+                >
+                  <Icon
+                    icon="ci:external-link"
+                    width="18"
+                    height="18"
+                    className="text-gray-100"
+                  />
+                  {link}
+                </Typography>
+
+                <div className="flex gap-1">
+                  {stack.map((tech) => (
+                    <span
+                      key={tech}
+                      className="px-3 py-1 rounded-full bg-white/10"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center w-full bg-gray-200 pb-24">
+              <img
+                src={image.path}
+                alt={image.alt}
+                width={image.width}
+                height={image.height}
+                className="bg-secondary-black px-5 pt-5 rounded-2xl"
+                style={{ marginTop: -imageH }}
+              />
             </div>
           </div>
-
-          <img
-            src="/images/orbiguide-screenshot.webp"
-            alt="OrbiGuide screenshot"
-            className="bg-secondary-black p-6 mt-12 rounded-2xl"
-          />
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
