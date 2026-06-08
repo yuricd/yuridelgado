eceb7d7df1855a606fe3c0513cbb630352b04787
Yuri <6846623+yuricd@users.noreply.github.com>
2026-06-08 18:28:37 +0100

    refactor: load site from astro config

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="unplugin-icons/types/react" />
 
 interface ImportMetaEnv {
-  readonly API_URL: string
+  readonly SITE: string;
+  readonly API_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
